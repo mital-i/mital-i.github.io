1b298a0f901c6228016ba047ca7f2d88db973876
Mitali Mittal <mitalim@uci.edu>
2024-12-28 15:57:06 -0800

    update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  //  <button onClick={() => setCount((count) => count + 1)}>
 
   return (
     <>
-      <div className="container">
-        <a href="https://www.linkedin.com/in/mitali-m/" target="_blank" rel="noopener noreferrer">linkedin</a>
-        <a href="https://drive.google.com/file/d/1jmB8i1-RCwH1_A9cUQRYdHzgsgIS4bhS/view?usp=drive_link" target="_blank" rel="noopener noreferrer">resume</a>
+     <div>
+      <h1>Mitali Mittal</h1>
+     </div>
+      <div className="footer"> {/* Added a container div */}
+        <div className="linkedin">
+          <a href="https://www.linkedin.com/in/mitali-m/" target="_blank" rel="noopener noreferrer">linkedin</a>
+        </div>
+        <div className="resume">
+          <a href="https://drive.google.com/file/d/1jmB8i1-RCwH1_A9cUQRYdHzgsgIS4bhS/view?usp=drive_link" target="_blank" rel="noopener noreferrer">resume</a>
+        </div>
+        <div className="youtube">
+          <a href="https://www.youtube.com/@mitali_com/featured" target="_blank" rel="noopener noreferrer">youtube</a>
+        </div>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      
     </>
   )
 }
